a24847118ec577059b9ca4f4d642b50274323416
Joe Slow <joe@slow.com>
2015-11-18 14:11:53 +0000

    More react

--- widgets/time.widget.js
+++ widgets/time.widget.js
@@ -1,31 +1,86 @@
 var moment = require('moment'),
     React = require('react');
 
-module.exports = {
-  dimensions: {
-    rows: 1,
-    columns: 1
-  },
-  template: {
-    element:
-      <div>
-        <span className="date">12/12/15</span>
-        <span className="time">12:12:13</span>
-      </div>
-    ,
-    css: '.date, .time { display: block; font-size: 40px; }',
-    model: {
-      time: new Date(),
-      date: new Date()
-    }
-  },
-  job: {
-    schedule: 1000,
-    script: function(emit) {
-      emit({
-        date: moment().format('h:mm:ss a').toString(),
-        time: moment().format('MMMM Do YYYY').toString()
-      });
-    }
+    var model = {};
+
+class Widget {
+
+  job(emit) {
+    emit({
+      date: moment().format('h:mm:ss a').toString(),
+      time: moment().format('MMMM Do YYYY').toString()
+    });
   }
+
+  getElement() {
+      return (
+        <div>
+          <span className="date">{this.model.date}</span>
+          <span className="time">12:12:13</span>
+        </div>
+      )
+  }
+
 }
+
+class TimeWidget extends Widget {
+
+
+    // dimensions: {
+    //   rows: 1,
+    //   columns: 1
+    // }
+    //
+    // template: {
+    //   element: React.createComponent(
+    //     <div>
+    //       <span className="date">{this.model.date}</span>
+    //       <span className="time">12:12:13</span>
+    //     </div>
+    //   ),
+    //   css: '.date, .time { display: block; font-size: 40px; }',
+    //   model: {
+    //     time: new Date(),
+    //     date: new Date()
+    //   }
+    // }
+    //
+    // job: {
+    //   schedule: 1000,
+    //   script: function(emit) {
+    //     emit({
+    //       date: moment().format('h:mm:ss a').toString(),
+    //       time: moment().format('MMMM Do YYYY').toString()
+    //     });
+    //   }
+    // }
+}
+
+TimeWidget.component = React.createClass({
+
+  propTypes: {
+    children: React.PropTypes.string
+  },
+
+  render: function() {
+    return (
+      <div>
+            <span className="date">{this.model.date}</span>
+            <span className="time">12:12:13</span>
+      </div>
+    );
+  }
+
+});
+
+
+TimeWidget.dimensions = {
+  rows: 1,
+  columns: 1
+}
+
+TimeWidget.css = ```
+  .date, .time { display: block; font-size: 40px; }
+```
+
+export default TimeWidget;
