037263ff3cefbb58fd1cc9af583d4af1f1f8c2c8
Antony Jones <aj@desirableobjects.co.uk>
2016-05-31 14:36:03 +0100

    @antony Final commit

--- src/views/helpers/valuerender.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(val) {
-
-  if (typeof val === 'string') {
-    return '\"'+val+'\"';
-  } else {
-    return JSON.stringify(val);
-  }
-
-}
--- widgets/stormpath.widget.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'contentful',
-  job: {
-    variables: {
-      name: 'Stormpath',
-      uri: 'https://status.stormpath.com'
-    }
-  }
-};
